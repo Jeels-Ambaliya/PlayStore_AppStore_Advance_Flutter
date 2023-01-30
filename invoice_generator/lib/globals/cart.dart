64a3b47c195e6dbbea968843a0809675da8b0bc3
Jeels-Ambaliya <19bmiit062@gmail.com>
2023-01-30 12:36:19 +0530

    Invoice Generator (Cloth App)

---
+++ invoice_generator/lib/globals/cart.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+class Cart {
+  late String? pic1;
+  late String? companyName;
+  late double? price;
+  late double? quePrice;
+  late double? quentity;
+
+  Cart({
+    required this.pic1,
+    required this.companyName,
+    required this.price,
+    required this.quePrice,
+    required this.quentity,
+  });
+}
