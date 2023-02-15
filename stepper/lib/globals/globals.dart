c87a7fa4fe91ed57c2cb2fc26560e126233facf1
Jeels Ambaliya <123535768+Jeels-Ambaliya@users.noreply.github.com>
2023-02-15 11:59:01 +0530

    Delete stepper directory

--- stepper/lib/globals/globals.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Globals{
-  static int stepIndex = 0;
-  static List<GlobalKey<FormState>> formKey = List.generate(3, (index) => GlobalKey<FormState>());
-
-  static DateTime? selectedDate;
-  static String date = "00/00/0000";
-}
